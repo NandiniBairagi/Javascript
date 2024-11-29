dfb4f12448172c54954940b73d6029450e5adbc1
NANDINI BAIRAGI <140041600+NandiniBairagi@users.noreply.github.com>
2024-11-29 10:24:05 +0000

    variables

---
+++ variables.js
@@ -0,0 +1,8 @@
+const accountId=42145
+let accountEmail="nandini@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+let accountstate;
+// accountId=2
+console.log(accountId)
+console.table([accountId,accountEmail,accountPassword,accountCity,accountstate])
